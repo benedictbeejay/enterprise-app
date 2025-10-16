c2ef3ee2cb2ae1e33fef4f0ebb97720c5f6f08c6
benedictbeejay <benedictbeejay@gmail.com>
2025-10-16 14:36:14 +0100

    Created Serving Unit list

---
+++ src/app/(dashboard)/admin/foods-managment/serving-units/_services/useQueries.ts
@@ -0,0 +1,22 @@
+import { useServingUnitsStore } from "../_libs/useServingUnitsStore";
+import { getServingUnit, getServingUnits } from "./services";
+import { useQuery } from "@tanstack/react-query";
+
+const useServingUnits = () => {
+  return useQuery({
+    queryKey: ["servingUnits"],
+    queryFn: getServingUnits,
+  });
+};
+
+const useServingUnit = () => {
+  const { selectedServingUnitId } = useServingUnitsStore();
+
+  return useQuery({
+    queryKey: ["servingUnits", { selectedServingUnitId }],
+    queryFn: () => getServingUnit(selectedServingUnitId!),
+    enabled: !!selectedServingUnitId,
+  });
+};
+
+export { useServingUnits, useServingUnit };
