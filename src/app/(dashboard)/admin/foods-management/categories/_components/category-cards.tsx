7e0d1c81e5bf50599e6aab5a3da4a16bdc4a76ec
benedictbeejay <benedictbeejay@gmail.com>
2025-10-27 21:06:13 +0100

    ..

--- src/app/(dashboard)/admin/foods-management/categories/_components/category-cards.tsx
+++ src/app/(dashboard)/admin/foods-management/categories/_components/category-cards.tsx
@@ -30,7 +30,8 @@ const CategoryCards = () => {
         <CategoryCardsSkeleton />
       ) : (
         <>
-          {categoriesQuery.data?.map((item) => (
+          {/* {categoriesQuery.data?.map((item) => ( */}
+          {categoriesQuery.data?.map((item: { id: number; name: string }) => (
             <div
               className="flex flex-col justify-between gap-3 rounded-lg border p-6"
               key={item.id}
