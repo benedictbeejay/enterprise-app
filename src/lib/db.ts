53498ccab32d86179ed09a441cba81c6ec7d5b0d
benedictbeejay <benedictbeejay@gmail.com>
2025-10-27 20:58:10 +0100

    ..

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,15 +1,33 @@
-import { PrismaClient } from "$/generated/prisma";
+// import { PrismaClient } from "$/generated/prisma";
+
+// const prismaClientSingleton = () => {
+//   return new PrismaClient();
+// };
+
+// declare const globalThis: {
+//   prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+// } & typeof global;
+
+// const db = globalThis.prismaGlobal ?? prismaClientSingleton();
+
+// export default db;
+
+// if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = db;
+
+import { PrismaClient } from "@prisma/client";
 
 const prismaClientSingleton = () => {
   return new PrismaClient();
 };
 
 declare const globalThis: {
-  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+  prismaGlobal?: ReturnType<typeof prismaClientSingleton>;
 } & typeof global;
 
 const db = globalThis.prismaGlobal ?? prismaClientSingleton();
 
-export default db;
+if (process.env.NODE_ENV !== "production") {
+  globalThis.prismaGlobal = db;
+}
 
-if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = db;
+export default db;
